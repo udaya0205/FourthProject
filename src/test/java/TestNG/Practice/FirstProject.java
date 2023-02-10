700c15fe62985c03686165ee4291ab7a3a885a03
udaya0205 <udaysubramaniam0205@gmail.com>
2023-02-11 00:18:51 +0530

    First Commit

---
+++ src/test/java/TestNG/Practice/FirstProject.java
@@ -0,0 +1,18 @@
+package TestNG.Practice;
+
+import org.testng.annotations.Test;
+
+public class FirstProject {
+	@Test
+	public void Browserlaunch() {
+		System.out.println("launch the browser");
+		
+		}
+	@Test 
+	
+	public void LoadUrlAddress() {
+		System.out.println("load url address");
+		
+		}
+
+}
